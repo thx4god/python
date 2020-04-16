45afd46709a1a3cc125ae4e768f37f48cc457d50
thx4god <thx4godsim@gmail.com>
2020-04-16 17:17:30 -0400

    처음으로 올리는 깃파일

---
+++ Boolean.py
@@ -0,0 +1,6 @@
+print(1==1)
+print(1==2)
+print(1<2)
+print(1>2)
+print(True) #  대문자를 구분한다.
+print(False)
---
+++ helloworld.py
@@ -0,0 +1,25 @@
+x = 10
+y = 5
+print(x+y)
+
+title = "python & ruby"
+print("Title is " + title)
+
+name = "egoing"
+print("안녕하세요." + name +"님!")
+print(name + "님을 위한 강의를 준비했습니다.")
+print( name + "님 꼭 참석 부탁드립니다." )
+
+# 장학재단 예제  후원금: 100만원, 학생수: 10명, 후원자: 20명  일때, 
+# print((100*10)/20)
+x = 100  # 후원금액
+y = 10   # 학생수
+z = 20   # 후원자수
+print((x*y)/z)
+
+# 문자(영문)로 변수작성
+donation = 200
+student = 10
+sponsor = 100
+print((donation*student)/sponsor)
+
